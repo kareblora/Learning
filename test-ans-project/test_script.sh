d3926edf558d839afbd8dceadda8a45278179b64
kareblora <kp.reblora@gmail.com>
2022-02-13 03:11:43 -0500

    Added service and lineinfile modules to ansible playbook

--- test-ans-project/test_script.sh
+++ test-ans-project/test_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 touch /root/dev_ops/from_controller/sample.txt
-echo "This is configured using the ansible controler" >> /root/dev_ops/from_controller/sample.txt 
+echo "This is configured using the ansible controller" >> /root/dev_ops/from_controller/sample.txt 
